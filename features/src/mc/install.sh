9f0c379cec0c2e42034c22b061ceabc8186802d8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-04-11 04:37:51 +0300

    fix(mc): Fix issues found by shellcheck

--- features/src/mc/install.sh
+++ features/src/mc/install.sh
@@ -4,10 +4,12 @@ set -e
 
 PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin
 
-if [ "$(id -u)" -ne 0 ]; then
+if [ "$(id -u || true)" -ne 0 ]; then
     echo 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
+
+: "${ENABLED:=}"
 
 if [ "${ENABLED}" = "true" ]; then
     echo '(*) Installing Midnight Commander...'

352ebf879928f08071771875fee486f169127d6e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-05-24 17:22:13 +0300

    refactor(wordpress): Decouple ES code from WordPress installer

--- features/src/wordpress/setup-wordpress.sh
+++ features/src/wordpress/setup-wordpress.sh
@@ -137,21 +137,6 @@ if ! wp core is-installed >/dev/null 2>&1; then
 
     wp user add-cap 1 view_query_monitor
 
-    if [ -e /etc/service/elasticsearch ] && wp cli has-command vip-search; then
-        echo "Waiting for Elasticsearch to come online..."
-        second=0
-        while ! curl -s 'http://127.0.0.1:9200/_cluster/health' > /dev/null && [ "${second}" -lt 60 ]; do
-            sleep 1
-            second=$((second+1))
-        done
-        status="$(curl -s 'http://127.0.0.1:9200/_cluster/health?wait_for_status=yellow&timeout=60s' | jq -r .status)"
-        if [ "${status}" != 'green' ] && [ "${status}" != 'yellow' ]; then
-            echo "WARNING: Elasticsearch has failed to come online"
-        fi
-
-        wp vip-search index --skip-confirm --setup
-    fi
-
     run-parts /var/lib/wordpress/postinstall.d
 else
     echo "WordPress already installed"

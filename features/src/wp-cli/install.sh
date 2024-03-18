a30d583ca898aff2bbb54d7d06a39b1d30757d31
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-03-18 11:52:52 +0200

    feat(wp-cli): add support for Debian-based distros

--- features/src/wp-cli/install.sh
+++ features/src/wp-cli/install.sh
@@ -9,26 +9,67 @@ if [ "$(id -u || true)" -ne 0 ]; then
     exit 1
 fi
 
-if [ -z "${_REMOTE_USER}" ] || [ "${_REMOTE_USER}" = "root" ]; then
-    USER=nginx
-else
-    USER="${_REMOTE_USER}"
-fi
-
 : "${NIGHTLY:=}"
 
 echo '(*) Installing wp-cli...'
+
 if [ "${NIGHTLY}" = "true" ]; then
     url="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar"
 else
     url="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 fi
 
-wget -q "${url}" -O /usr/local/bin/wp
-chmod 0755 /usr/local/bin/wp
+if ! hash wget >/dev/null 2>&1; then
+    : "${ID:=}"
+    : "${ID_LIKE:=${ID}}"
 
-if [ -n "$(command -v php || true)" ]; then
-    sudo -u "${USER}" wp cli info || true
+    if [ -z "${ID}" ]; then
+        echo 'Unable to determine the distribution.'
+        exit 1
+    fi
+
+    case "${ID_LIKE}" in
+        "debian")
+            PACKAGES="wget"
+            if ! hash update-ca-certificates >/dev/null 2>&1; then
+                PACKAGES="${PACKAGES} ca-certificates"
+            fi
+
+            apt-get update
+            # shellcheck disable=SC2086
+            apt-get install -y --no-install-recommends ${PACKAGES}
+
+            wget -q "${url}" -O /usr/local/bin/wp
+
+            # shellcheck disable=SC2086
+            apt-get purge -y --auto-remove ${PACKAGES}
+            apt-get clean
+            rm -rf /var/lib/apt/lists/*
+        ;;
+
+        "alpine")
+            PACKAGES="wget"
+            if ! hash update-ca-certificates >/dev/null 2>&1; then
+                PACKAGES="${PACKAGES} ca-certificates"
+            fi
+
+            # shellcheck disable=SC2086
+            apk add --no-cache ${PACKAGES}
+
+            wget -q "${url}" -O /usr/local/bin/wp
+
+            # shellcheck disable=SC2086
+            apk del --no-cache ${PACKAGES}
+        ;;
+
+        *)
+            echo "(!) Unsupported distribution: ${ID}"
+            exit 1
+        ;;
+    esac
+else
+    wget -q "${url}" -O /usr/local/bin/wp
 fi
 
+chmod 0755 /usr/local/bin/wp
 echo 'Done!'
